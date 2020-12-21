0794cd940175ad3f067e41deac04d9b5f671e789
hayashi <hayashi@source-pod.com>
2020-12-21 10:17:54 +0900

    first commit

---
+++ xxx_sample01/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2020- Hiroki.H.
+
+__all__ = [
+    XSample01,
+]
+
+
+from .xxx_sample01 import XSample01
---
+++ xxx_sample01/xxx_sample01.py
@@ -0,0 +1,7 @@
+# Copyright 2020- Hiroki.H.
+
+
+class XSample01(object):
+    def __init__(self):
+        pass
+    pass
